7361e858ff11c921b6493f1476876fc78f766df0
Ryousuke-Tanaka <sample@email.com>
2020-04-11 23:13:39 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # ページごとのタイトルを返す
+  def full_title(page_name = "")
+    base_title = "Attendance_A_App"
+    if page_name.empty?
+      base_title
+    else
+      page_name + "|" + base_title
+    end
+  end
 end
