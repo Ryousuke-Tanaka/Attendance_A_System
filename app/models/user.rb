9a6d14e91e41dc6d506d5bc38e1fd15d740ad810
Ryousuke-Tanaka <sample@email.com>
2020-04-18 08:19:23 +0000

    一般ユーザー勤怠画面大枠完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :attendances, dependent: :destroy
   # 「remember_token」という仮想の属性を作成
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
@@ -8,10 +9,10 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 100 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
-  validates :department, length: { in: 2..30 }, allow_blank: true
+  validates :affiliation, length: { in: 2..30 }, allow_blank: true
   validates :basic_time, presence: true
-  validates :start_work_time, presence: true
-  validates :finish_work_time, presence: true
+  validates :designated_work_start_time, presence: true
+  validates :designated_work_end_time, presence: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
--- db/migrate/20200417140129_add_department_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddDepartmentToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :department, :string, default: "未所属" 
-  end
-end
