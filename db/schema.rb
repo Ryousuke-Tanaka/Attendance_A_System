315bbc02a75e56ece1de735778556c7ec0600f76
Ryousuke-Tanaka <sample@email.com>
2020-04-26 04:06:22 +0000

    work_time追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200426001958) do
+ActiveRecord::Schema.define(version: 20200421140234) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -21,9 +21,6 @@ ActiveRecord::Schema.define(version: 20200426001958) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "spread_day", default: false
-    t.datetime "estimated_finished_at"
-    t.string "Job_description"
-    t.string "own_superior"
     t.index ["user_id"], name: "index_attendances_on_user_id"
   end
 
