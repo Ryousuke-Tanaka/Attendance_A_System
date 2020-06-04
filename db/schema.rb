ff2746ffe6461ab12ac5c52135b1f768bdee0df1
Ryousuke-Tanaka <sample@email.com>
2020-06-04 00:46:51 +0000

    dbにbefore_started_atなどを変更前を追加

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,8 @@ ActiveRecord::Schema.define(version: 20200523235516) do
     t.date "worked_on"
     t.datetime "started_at"
     t.datetime "finished_at"
+    t.datetime "before_started_at"
+    t.datetime "before_finished_at"
     t.string "note"
     t.integer "user_id"
     t.datetime "created_at", null: false
@@ -49,10 +51,10 @@ ActiveRecord::Schema.define(version: 20200523235516) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "affiliation", default: "未所属"
-    t.datetime "basic_time", default: "2020-05-31 23:00:00"
-    t.datetime "work_time", default: "2020-05-31 22:30:00"
-    t.datetime "designated_work_start_time", default: "2020-06-01 00:00:00"
-    t.datetime "designated_work_end_time", default: "2020-06-01 09:00:00"
+    t.datetime "basic_time", default: "2020-06-03 23:00:00"
+    t.datetime "work_time", default: "2020-06-03 22:30:00"
+    t.datetime "designated_work_start_time", default: "2020-06-04 00:00:00"
+    t.datetime "designated_work_end_time", default: "2020-06-04 09:00:00"
     t.boolean "superior", default: false
     t.integer "employee_number"
     t.string "uid"
