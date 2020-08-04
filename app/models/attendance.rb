62db6d265385751454cd3e7101ecb44f0db201ef
Ryousuke-Tanaka <sample@email.com>
2020-08-04 11:32:28 +0000

    完成20200804_02

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -29,7 +29,7 @@ class Attendance < ApplicationRecord
   
   
   def finished_at_is_invalid_without_a_started_at
-    errors.add(:started_at, "が必要です") if (started_at.blank? && finished_at.present?)  || (before_finished_at.blank? && before_finished_at.present?) || (after_finished_at.blank? && after_finished_at.present?)
+    errors.add(:started_at, "が必要です") if (started_at.blank? && finished_at.present?)  || (before_started_at.blank? && before_finished_at.present?) || (after_started_at.blank? && after_finished_at.present?)
   end
   
   def started_at_is_invalid_without_a_finished_at
